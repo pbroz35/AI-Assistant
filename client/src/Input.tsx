9f0fc1065e39d4f0ea076262531628ce8dd54018
Piotr Brozek <1brozek.peter@gmail.com>
2024-07-15 22:12:41 -0500

    added icon to input bar and fixed styling issues w/ bar

--- client/src/Input.tsx
+++ client/src/Input.tsx
@@ -1,10 +1,17 @@
+import { FaSearch } from 'react-icons/fa';
+
+
 const Input = () => {
     return (
         
 
         <div className="search-container">
         <form className="search">
+          
           <input type="text" placeholder="Message AI" name="search" />
+          <FaSearch className="search-icon"></FaSearch>
+          
+        
         </form>
       </div>
 
